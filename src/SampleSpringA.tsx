8fb176159c0fc2b726d3517145e9cd892ac7e82a
ShutoYamada <stym1122@gmail.com>
2022-03-14 17:31:49 +0900

    SampleSpringAを追加

---
+++ src/SampleSpringA.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
+
+export default () => {
+  const [enter, setEnter] = useState(false);
+  const spring = useSpring({
+    fontSize: enter ? "48pt" : "24pt",
+    color: enter ? "red" : "green",
+  });
+
+  return (
+    <animated.div
+      style={spring}
+      onMouseEnter={(e) => setEnter(true)}
+      onMouseLeave={(e) => setEnter(false)}
+    >
+      Hello React Spring.
+    </animated.div>
+  );
+};
